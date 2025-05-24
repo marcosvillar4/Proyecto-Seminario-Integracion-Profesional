f822aee88c8ce552775564e2d24aa37d45ecf32c
BuildTools <unconfigured@null.spigotmc.org>
2025-05-24 10:36:22 -0300

    Mas Hobbies

--- app/src/main/java/tpo/seminario/breakbuddy/util/HobbiesList.kt
+++ app/src/main/java/tpo/seminario/breakbuddy/util/HobbiesList.kt
@@ -3,6 +3,9 @@ package tpo.seminario.breakbuddy.util
 object HobbiesList {
     val DEFAULT = listOf(
         "Leer", "Hacer ejercicio", "Cocinar", "Escuchar música",
-        "Pintar", "Jardinería", "Videojuegos", "Meditar", "Bailar", "Ver películas"
+        "Pintar", "Jardinería", "Videojuegos", "Meditar", "Bailar", "Ver películas" ,
+        "Tocar un instrumento musical" , "Fotografía", "Viajar", "Aprender idiomas" ,
+        "Manualidades", "Juegos de mesa", "Actuar", "Camping", "Creación de contenido",
+        "Pescar"
     )
 }
