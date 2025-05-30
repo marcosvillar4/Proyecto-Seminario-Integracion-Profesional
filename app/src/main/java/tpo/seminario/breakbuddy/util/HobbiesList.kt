7990cd16e49296b10b593695f04309b0c207ec78
santi <santimussi1959@gmail.com>
2025-05-30 20:27:18 -0300

    New Hobbies

--- app/src/main/java/tpo/seminario/breakbuddy/util/HobbiesList.kt
+++ app/src/main/java/tpo/seminario/breakbuddy/util/HobbiesList.kt
@@ -6,6 +6,12 @@ object HobbiesList {
         "Pintar", "Jardinería", "Videojuegos", "Meditar", "Bailar", "Ver películas" ,
         "Tocar un instrumento musical" , "Fotografía", "Viajar", "Aprender idiomas" ,
         "Manualidades", "Juegos de mesa", "Actuar", "Camping", "Creación de contenido",
-        "Pescar"
+        "Pescar",
+
+        // Nuevos hobbies añadidos
+        "Andar en bicicleta", "Escalada", "Skate", "Armar rompecabezas", "Modelismo",
+        "Escribir", "Bloggear", "Canto", "Programar", "Diseño gráfico",
+        "Correr", "Nadar", "Trekking", "Ajedrez", "Astronomía",
+        "Voluntariado", "Restaurar muebles", "Tejer", "Cerámica", "Basket", "Volley", "Golf"
     )
 }
