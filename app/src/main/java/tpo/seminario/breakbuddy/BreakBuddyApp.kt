b46f0d0a911eaae257b44e49a9d010167a04cb32
Miqueas Vanore <miqueasvn@gmail.com>
2025-06-10 21:19:21 -0300

    prueba 3

--- app/src/main/java/tpo/seminario/breakbuddy/BreakBuddyApp.kt
+++ app/src/main/java/tpo/seminario/breakbuddy/BreakBuddyApp.kt
@@ -63,7 +63,7 @@ class BreakBuddyApp : Application() {
             Firebase.firestore.useEmulator("10.0.2.2", 8080)
             Log.d("BreakBuddyApp", "Connected to Firebase Emulators")
         }
-        //prueba
+        //prueba 3
 }
 }
 
